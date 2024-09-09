dcb6fc85427ce232b366fece59d34d407e64aa4d
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-09 11:15:38 +0300

    solved

--- main.js
+++ main.js
@@ -33,3 +33,11 @@ for(let i=0 ; i<10 ; i++){
     secondNumber = nextNumber;
 }
 
+// Question No5
+ 
+for (let i =1; i <= 3 ; i++) {
+  for (let j =1; j <= 10 ; j++) {
+    console.log(`${i} * ${j} =` + i * j)
+  }
+
+}
