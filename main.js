f8111eb6c8c14a45184b9c269f7615eafb2d2cf1
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-09 10:58:39 +0300

    Question No3 is solved

--- main.js
+++ main.js
@@ -20,3 +20,12 @@ for(let i=0 ; i< 25 ; i++){
 for(let i=0 ; i<=10 ; i++){
     console.log(i * i);
 }
+
+// Question No3
+for(let i=0 ; i<20 ; i++){
+    if(i % 2 == 0){
+        continue;
+    }else{
+        console.log(i);
+    }
+}
